bc2b1515cc8bd811dea3741551467315f6a442ca
James Hafner <james@jameshafner.com>
2011-02-19 17:37:11 -0600

    Further setup foundation.

---
+++ random-ajax-slider.jquery.js
@@ -0,0 +1,36 @@
+/**
+ * Random Ajax Slider
+ *
+ * Description: Creates a slider out of a list of <li> elements, randomizes them, and ajax loads them based on how likely they are to appear in the viewport.
+ *
+ * Usage: 
+ *
+ *	$('#gallery').imgSwap({
+ *		'randomize'	: 'true'
+ *	});
+ *
+ * Defaults:
+ *
+ *		'randomize'	: 'true'
+ *
+ *
+ * Dual licensed under MIT and GNU General Public License version 3 (GPLv3)
+ * http://www.opensource.org/licenses
+ * Launch  : October 2010
+ */
+(function($) {
+	$.fn.exists = function(){if(jQuery(this).length == 0){return false;}else{return true;}};
+	$.fn.ras = function(options) {
+
+		var defaults = {
+		
+			'randomize'	: 'true'
+		
+		};
+		
+		return this.each(function() {
+			//Stuff
+		});
+	}
+
+})(jQuery);
